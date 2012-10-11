139c774f8930f2f84e617bbdbb6c7d501368178d
Simon Guillem-Lessard <s.g.lessard@gmail.com>
2012-10-11 16:54:02 -0400

    New Instagram melody (notice: needs sglessard/OAuthBase 16aec449ab4c0abd0ae329e446dca3f6915ea396 commit)

---
+++ lib/melody/sfInstagramMelody.class.php
@@ -0,0 +1,36 @@
+<?php
+class sfInstagramMelody extends sfMelody2
+{
+  protected function initialize($config)
+  {
+    $this->setRequestAuthUrl('https://instagram.com/oauth/authorize');
+    $this->setAccessTokenUrl('https://api.instagram.com/oauth/access_token');
+    $this->setAccessTokenMethod('POST');
+    
+    if(isset($config['scope']))
+    {
+      $this->setAuthParameter('scope', implode(',', $config['scope']));
+    }
+  }
+
+  public function initializeFromToken($token)
+  {
+    if($token && $token->getStatus() == Token::STATUS_ACCESS)
+    {
+      $this->setAlias('me','users/self');
+    }
+  }
+
+  public function getIdentifier()
+  {
+    return $this->getToken()->getParam('id');
+  }
+
+  protected function setExpire(&$token)
+  {
+    if($token->getParam('expires'))
+    {
+      $token->setExpire(time() + $token->getParam('expires'));
+    }
+  }
+}
